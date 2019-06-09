37b2a50c56629e5b41d0fa237ef240e3d4934089
DZF <dzfischer@gmail.com>
2019-06-09 12:52:59 -0400

    reorganization

--- code/0_analysis.py
+++ code/0_analysis.py
@@ -4,16 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# grabbing the hdf5_getters.py file stored alongside analysis.py
+# we use it to save us major time converting from the h5 files
+# into a data format that we can use
 import hdf5_getters as hg
 
+# this step takes a little while, so, print
 print('Done importing')
 
-# -----------------
-
-DEBUG = 1
+# Debug section; either True or False (1 or 0)
+DEBUG = True
 test_length = 50
-
-# -----------------
 
 # Collection of methods from hdf5_getters we want to use en masse
 hg_methods = [
@@ -22,10 +23,8 @@ hg_methods = [
     hg.get_duration,
     hg.get_title]
 
+# Initializing datatypes we'll need later
 big_data = []
-
-
-
 df_songs = pd.DataFrame(index =hg_methods) 
 print(df_songs)
 k = 0
@@ -33,35 +32,33 @@ count = 0
 data = []
 
 file_no = 0
-#df_studies['proportion of red'] = redarray
-#df_studies['proportion of red as percent'] = redpercentarray
-#df_studies['proportion of blue area'] = bluearray
-#df_studies['proportion of blue area as percent'] = bluepercentarray
-#df_studies
 
 
 # print(f'Target directory: {f'{os.getcwd()}/data/MillionSongSubset/data'} and type : {type(os.getcwd())}')
 
 
 # Main loop to walk through all songs; uses users project install directory, but must be changed if we edit where the millionsong dataset is located
-for subdir, dirs, files in os.walk(f'{os.getcwd()}/data/MillionSongSubset/data'):
+for subdir, dirs, files in os.walk(f'{os.getcwd()}/../MillionSongSubset/data'):
     for file in files:
 
         hfile = hg.open_h5_file_read(os.path.join(subdir, file))
 
-        for method in hg_methods:
-            data.append(method(hfile))
-            df_songs.head()   
-#            df_songs = df_songs.append(pd.Series(data, index=df_songs.columns ), ignore_index=True)
-#            print(data)
-            data = []
+        # for method in hg_methods:
+            # data.append(method(hfile))
+            # df_songs.head()   
+            # df_songs = df_songs.append(pd.Series(data, index=df_songs.columns ), ignore_index=True)
+            # print(data)
+            # data = []
+        print(file_no)
         file_no +=1
 
 
         hfile.close()
-        if (file_no > test_length) & (DEBUG == 1):
+
+        # exit file nested loop if we're in debug mode
+        if (file_no > test_length) & DEBUG:
             break
-    if (file_no > test_length) & (DEBUG == 1):
+    if (file_no > test_length) & DEBUG:
         break
 #        if k > 1:
 #            k = hg.get_num_songs(hfile)
@@ -82,22 +79,3 @@ for subdir, dirs, files in os.walk(f'{os.getcwd()}/data/MillionSongSubset/data')
  #               f'Full: {os.path.join(subdir, file)}\n'
  #               f'subdir: {subdir}\n'
   #              f'file: {file}')
-
-
-
-#h5 = hg.open_h5_file_read("data/MillionSongSubset/data/A/A/A/TRAAAAW128F429D538.h5")
-
-# #duration = hg.get_duration(h5)
-# #print(f'duration: {duration}')
-
-# location = hg.get_artist_location(h5)
-# print(f'location: {location}')
-
-# num_songs = hg.get_num_songs(h5)
-# print(f'num songs: {num_songs}')
-
-# artist_name = hg.get_artist_name(h5)
-# print(f'artist name: {artist_name}')
-# print(type(artist_name))
-
-# h5.close()
