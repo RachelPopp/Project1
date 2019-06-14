ae0f11cbf8bf1d2774d27d43869e3bf0729719e9
DZF <dzfischer@gmail.com>
2019-06-14 17:00:51 -0400

    error checking when saving

--- code/save_df.py
+++ code/save_df.py
@@ -1,7 +1,16 @@
 import os, csv
 
+# Just saving a given df to given filename; relative location fixed
 def save (df_songs, filename):
     with open(os.path.join('..','code_output',filename), 'w') as output_file:
         writer = csv.writer(output_file)
-        df_songs.to_csv(output_file, encoding='utf-8')
-        print(f"Saved to: {os.path.join('..','code_output',filename)}")
+        
+        # Save... if it works.
+        try:
+            df_songs.to_csv(output_file, encoding='utf-8')
+            # little confirmation message when done saving
+            print(f"Saved to: {os.path.join('..','code_output',filename)}")
+        except:
+            # If an error, say so and print what it was
+            print(f"ERROR! 'df_songs' NOT SAVED TO {filename}")
+            print(f"Error type: {sys.exc_info()[0]}")
